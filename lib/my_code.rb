de730831766c1b100e6b024fab075a2f3111c05a
Rikesh Balakrishnan <rik280491@googlemail.com>
2019-11-22 17:44:05 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # My Code here....
+def map_to_negativize(source_array)
+  i = 0 
+   while i < source_array.length do
+    
+end 
+  
+
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  
+end 
+
+def map_to_square(source_array)
+  
+end 
+
+
